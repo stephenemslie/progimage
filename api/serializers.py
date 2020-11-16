b405c06fe2b4015dd831944163674bc5bb5645ca
Stephen Emslie <stephenemslie@gmail.com>
2020-11-16 15:37:03 +0200

    Added a detail action to rotate an image
    This accepts the `degrees` parameter and performs validation to make
    sure it is an integer.

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import Image
 
 
-class ImageSerializer(ModelSerializer):
+class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = ['id', 'created', 'image']
+
+
+class RotateSerializer(serializers.Serializer):
+    degrees = serializers.IntegerField()
--- api/views.py
+++ api/views.py
@@ -1,10 +1,29 @@
+import io
+
+import PIL
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Image
-from .serializers import ImageSerializer
+from .serializers import ImageSerializer, RotateSerializer
 
 
 class ImageViewSet(ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+
+    @action(detail=True, methods=['post'])
+    def rotate(self, request, pk=None):
+        image = self.get_object()
+        pil_image = PIL.Image.open(image.image)
+        serializer = RotateSerializer(data=request.POST)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+        pil_image.rotate(serializer.validated_data['degrees'])
+        with io.BytesIO() as f:
+            pil_image.save(f, format=pil_image.format)
+        image = self.get_object()
+        serializer = self.get_serializer(image)
+        return Response(serializer.data)
