9cf77d661840e2b1b0425aaf927387557a860b75
Stephen Emslie <stephenemslie@gmail.com>
2020-11-16 12:48:09 +0200

    Handle static media files in DEBUG mode

--- progimage/urls.py
+++ progimage/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
 from api.views import ImageViewSet
@@ -7,6 +9,10 @@ from api.views import ImageViewSet
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 router = DefaultRouter()
 router.register('images', ImageViewSet)
