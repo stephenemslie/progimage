65406bae6994ef721b9d1707d9bd0c96ba3e5ae4
Stephen Emslie <stephenemslie@gmail.com>
2020-11-17 09:19:48 +0200

    Create a .env file if none is present

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f .env ]; then
+    PW=`base64 /dev/urandom | head -c 50`
+    echo "SECRET_KEY=$PW" > .env
+fi
+
 if [ "$1" = 'runserver' ]; then
     pipenv run ./manage.py migrate
     exec pipenv run ./manage.py runserver 0:8000
